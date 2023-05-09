4e02f7c9b81f972db56382737a21ba539d5f2d6b
CristianGonzalez07 <1995.cgonzalez.07@gmail.com>
2023-05-09 16:41:02 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+export default {
+  mode: 'jit',
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
