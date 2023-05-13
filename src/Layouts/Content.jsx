f018865832391258d2afee9797ef850f651ad323
CristianGonzalez07 <1995.cgonzalez.07@gmail.com>
2023-05-13 19:27:23 -0300

    button fixed

--- src/Layouts/Content.jsx
+++ src/Layouts/Content.jsx
@@ -5,15 +5,20 @@ const Content = ({title, InnerContent}) => {
   const {user, logout} = useAuth()
   return (
     <>
-      <div className='h-[15%] lg:h-[20%] flex justify-center items-center'>
-        <h1 className='text-2xl lg:text-6xl text-white font-bold'>{title}</h1>
+      <div className='h-[15%] lg:h-[20%]'>
+        <div className="h-[50%] flex justify-start items-center p-3">
+          {user &&  <button 
+            className='text-white bg-green-500 rounded-lg p-3 ml-1 text-sm lg:text-lg font-bold flex items-center'
+            onClick={() => logout()} 
+          >
+            Cerrar Sesión
+          </button>}
+        </div>
+        <div className='flex justify-center items-center'>
+          <h1 className='text-2xl lg:text-6xl text-white font-bold'>{title}</h1>
+        </div>
       </div>
-      {user && <button 
-        className='absolute top-0 right-0 text-white bg-green-500 rounded-lg p-3 ml-1 text-sm lg:text-lg font-bold flex items-center m-5'
-        onClick={() => logout()} 
-      >
-        Cerrar Sesión
-      </button>}
+
       <div className='h-[85%] lg:h-[80%] flex justify-center'>
         <div className='bg-bg-secondary w-[90%] lg:w-[70%] h-[90%] rounded-lg shadow-lg'>
           {InnerContent}       
